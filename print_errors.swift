ca15babf55afbc602bb298e03a98d284490397db
Joy Jin <jojin@davidson.edu>
2026-07-12 18:34:45 +0800

    Remove committed Swift scratch files

--- print_errors.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-enum HAWebSocketClientError: Error, Equatable {
-    case invalidOutboundMessage
-    case invalidInboundMessage
-    case missingRequestID
-    case authRequiredExpected
-}
-print((HAWebSocketClientError.invalidInboundMessage as NSError).description)
-print((HAWebSocketClientError.invalidOutboundMessage as NSError).description)
